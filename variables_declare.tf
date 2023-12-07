a6a5cbd622bc61d3961cc7f1cf8b36036161d3ad
maria-ilinca <92468249+maria-ilinca@users.noreply.github.com>
2023-12-07 11:30:43 +0200

    Update variables_declare.tf

--- variables_declare.tf
+++ variables_declare.tf
@@ -1,11 +1,55 @@
-variable "client_id" {
-  description = "Azure AD Application Client ID"
+# storing the variables
+
+variable "resource_group_name" {
+  description = "Name of the Azure resource group"
+  type        = string
+  default     = "example-resources"
 }
 
-variable "client_secret" {
-  description = "Azure AD Application Client Secret"
+variable "location" {
+  description = "Azure region for resources"
+  type        = string
+  default     = "East Europe"  # Change to your preferred region
 }
 
-variable "tenant_id" {
-  description = "Azure AD Directory (Tenant) ID"
+variable "vnet_name" {
+  description = "Name of the Azure virtual network"
+  type        = string
+  default     = "example-vnet"
+}
+
+variable "public_subnet_name" {
+  description = "Name of the public subnet"
+  type        = string
+  default     = "public-subnet"
+}
+
+variable "private_subnet_name" {
+  description = "Name of the private subnet"
+  type        = string
+  default     = "private-subnet"
+}
+
+variable "vm_size" {
+  description = "Size of the virtual machine in the public subnet"
+  type        = string
+  default     = "Standard_DS1_v2"
+}
+
+variable "admin_username" {
+  description = "Admin username for the virtual machine"
+  type        = string
+  default     = "adminuser"
+}
+
+variable "admin_password" {
+  description = "Admin password for the virtual machine"
+  type        = string
+  default     = "AdminPassword1234!" 
+}
+
+variable "allowed_public_ip" {
+  description = "Public IP allowed to access Apache"
+  type        = string
+  default     = "<allowed_public_ip>"  
 }
