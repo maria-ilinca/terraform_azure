6d29e110229cec3068691f5d1031d48a7ba54874
maria-ilinca <92468249+maria-ilinca@users.noreply.github.com>
2023-12-11 12:11:42 +0200

    Create install_phpmyadmin.sh

---
+++ install_phpmyadmin.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+sudo apt-get update
+
+sudo apt-get install -y apache2 php php-mbstring php-gettext libapache2-mod-php php-mysql
+
+sudo apt-get install -y phpmyadmin
+
+echo "Include /etc/phpmyadmin/apache.conf" | sudo tee -a /etc/apache2/apache2.conf
+
+sudo systemctl restart apache2
+sudo systemctl enable apache2
