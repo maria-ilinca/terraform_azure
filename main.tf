213b9e6fda44ccabcfc5c47787221aed64cdf057
maria-ilinca <92468249+maria-ilinca@users.noreply.github.com>
2023-12-11 12:14:04 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,80 +1,135 @@
 # Configure the Microsoft Azure Provider
 provider "azurerm" {
-
   skip_provider_registration = true 
   features = {}
-
 }
 
 # Create a resource group
-resource "azurerm_resource_group" "example" {
-  name     = "example-resources"
+resource "azurerm_resource_group" "ilinca" {
+  name     = "ilinca"
   location = "East Europe"
 }
 
 # Create a virtual network within the resource group
-resource "azurerm_virtual_network" "example" {
-  name                = "example-vnet"
-  resource_group_name = azurerm_resource_group.example.name
-  location            = azurerm_resource_group.example.location
+resource "azurerm_virtual_network" "ilinca" {
+  name                = "ilinca-vnet"
+  resource_group_name = azurerm_resource_group.ilinca.name
+  location            = azurerm_resource_group.ilinca.location
   address_space       = ["10.0.0.0/16"]
 }
 
 # Subnet Public
 resource "azurerm_subnet" "public" {
   name                 = "public-subnet"
-  resource_group_name  = azurerm_resource_group.example.name
-  virtual_network_name = azurerm_virtual_network.example.name
+  resource_group_name  = azurerm_resource_group.ilinca.name
+  virtual_network_name = azurerm_virtual_network.ilinca.name
   address_prefixes     = ["10.0.1.0/24"]
 }
 
 # Subnet Private
 resource "azurerm_subnet" "private" {
   name                 = "private-subnet"
-  resource_group_name  = azurerm_resource_group.example.name
-  virtual_network_name = azurerm_virtual_network.example.name
+  resource_group_name  = azurerm_resource_group.ilinca.name
+  virtual_network_name = azurerm_virtual_network.ilinca.name
   address_prefixes     = ["10.0.2.0/24"]
 }
 
 # Network Security Group for VM
-resource "azurerm_network_security_group" "example" {
-  name                = "example-nsg"
-  resource_group_name = azurerm_resource_group.example.name
+resource "azurerm_network_security_group" "ilinca" {
+  name                = "ilinca-nsg"
+  resource_group_name = azurerm_resource_group.ilinca.name
 }
 
 # Network Interface for VM
-resource "azurerm_network_interface" "example" {
-  name                = "example-nic"
-  resource_group_name = azurerm_resource_group.example.name
+resource "azurerm_network_interface" "ilinca" {
+  name                = "ilinca-nic"
+  resource_group_name = azurerm_resource_group.ilinca.name
 
   ip_configuration {
-    name                          = "example-nic-configuration"
+    name                          = "ilinca-nic-configuration"
     subnet_id                     = azurerm_subnet.public.id
     private_ip_address_allocation = "Dynamic"
   }
 }
 
-# Virtual Machine
-resource "azurerm_virtual_machine" "example" {
-  name                  = "example-vm"
-  resource_group_name   = azurerm_resource_group.example.name
-  location              = azurerm_resource_group.example.location
+# Database Server in Subnet Private
+resource "azurerm_virtual_machine" "ilinca_database" {
+  name                  = "ilinca-database-vm"
+  resource_group_name   = azurerm_resource_group.ilinca.name
+  location              = azurerm_resource_group.ilinca.location
   size                  = "Standard_DS1_v2"
-  admin_username        = "adminuser"
-  admin_password        = "AdminPassword1234!"
-  network_interface_ids = [azurerm_network_interface.example.id]
+  admin_username        = "dbadmin"
+  admin_password        = "DBPassword1234!" 
+  network_interface_ids = [azurerm_network_interface.ilinca_database.id]
 
   os_profile {
-    computer_name  = "example-vm"
-    admin_username = "adminuser"
+    computer_name  = "ilinca-database-vm"
+    admin_username = "dbadmin"
   }
 
   os_profile_linux_config {
-    disable_password_authentication = true
-    ssh_keys {
-      path     = "/home/adminuser/.ssh/authorized_keys"
-      key_data = var.ssh_public_key
-    }
+    disable_password_authentication = false
+  }
+
+  source_image_reference {
+    publisher = "Canonical"
+    offer     = "UbuntuServer"
+    sku       = "16.04-LTS"
+    version   = "latest"
+  }
+}
+
+# Network Interface for Database VM
+resource "azurerm_network_interface" "ilinca_database" {
+  name                = "ilinca-database-nic"
+  resource_group_name = azurerm_resource_group.ilinca.name
+
+  ip_configuration {
+    name                          = "ilinca-database-nic-configuration"
+    subnet_id                     = azurerm_subnet.private.id
+    private_ip_address_allocation = "Dynamic"
+  }
+}
+
+# SQL Server
+resource "azurerm_sql_server" "ilinca_sql_server" {
+  name                         = "ilinca-sql-server"
+  resource_group_name          = azurerm_resource_group.ilinca.name
+  location                     = azurerm_resource_group.ilinca.location
+  version                      = "12.0"
+  administrator_login          = "sqladmin"
+  administrator_login_password = "SqlAdminPassword1234!"
+}
+
+# SQL Database
+resource "azurerm_sql_database" "ilinca_sql_db" {
+  name                        = "ilinca-sql-db"
+  resource_group_name         = azurerm_resource_group.ilinca.name
+  location                    = azurerm_resource_group.ilinca.location
+  server_name                 = azurerm_sql_server.ilinca_sql_server.name
+  edition                     = "Standard"
+  collation                   = "SQL_Latin1_General_CP1_CI_AS"
+  max_size_gb                 = 1
+  requested_service_objective = "S0"
+}
+
+# Virtual Machine
+resource "azurerm_virtual_machine" "ilinca" {
+  name                  = "ilinca-vm"
+  resource_group_name   = azurerm_resource_group.ilinca.name
+  location              = azurerm_resource_group.ilinca.location
+  size                  = "Standard_DS1_v2"
+  admin_username        = "admin"
+  admin_password        = "AdminPassword1234!"
+  network_interface_ids = [azurerm_network_interface.ilinca.id]
+
+  os_profile {
+    computer_name  = "ilinca-vm"
+    admin_username = "admin"
+  }
+
+  os_profile_linux_config {
+    disable_password_authentication = false
   }
 
   source_image_reference {
@@ -86,16 +141,16 @@ resource "azurerm_virtual_machine" "example" {
 }
 
 # Apache VM Extension
-resource "azurerm_virtual_machine_extension" "apache" {
+resource "azurerm_virtual_machine_extension" "phpmyadmin" {
   name                 = "customScript"
-  virtual_machine_id   = azurerm_virtual_machine.example.id
+  virtual_machine_id   = azurerm_virtual_machine.ilinca.id
   publisher            = "Microsoft.Azure.Extensions"
   type                 = "CustomScript"
   type_handler_version = "2.0"
 
   settings = <<SETTINGS
     {
-        "script": "./install_apache_php.sh"
+        "script": "./install_phpmyadmin.sh"
     }
 SETTINGS
 
@@ -104,46 +159,11 @@ SETTINGS
 PROTECTED_SETTINGS
 }
 
-# Custom Script for Apache and PHP installation
-resource "azurerm_storage_account" "script_storage" {
-  name                     = "scriptstorage${random_string.random_suffix.result}"
-  resource_group_name      = azurerm_resource_group.example.name
-  location                 = azurerm_resource_group.example.location
-  account_tier             = "Standard"
-  account_replication_type = "LRS"
-}
-
-resource "random_string" "random_suffix" {
-  length  = 6
-  special = false
-  upper   = false
-}
-
-resource "azurerm_storage_container" "script_container" {
-  name                  = "scriptcontainer"
-  storage_account_name  = azurerm_storage_account.script_storage.name
-  container_access_type = "private"
-}
-
-resource "azurerm_storage_blob" "install_script" {
-  name                   = "install_apache_php.sh"
+# Custom Script for PHPMyAdmin installation
+resource "azurerm_storage_blob" "install_phpmyadmin_script" {
+  name                   = "install_phpmyadmin.sh"
   storage_account_name   = azurerm_storage_account.script_storage.name
   storage_container_name = azurerm_storage_container.script_container.name
   type                   = "Block"
-  content                = file("${path.module}/install_apache_php.sh")
-}
-
-# Network Security Group Rule for Apache
-resource "azurerm_network_security_rule" "allow_apache" {
-  name                        = "allow_apache"
-  priority                    = 1001
-  direction                   = "Inbound"
-  access                      = "Allow"
-  protocol                    = "Tcp"
-  source_port_range           = "*"
-  destination_port_range      = 80
-  source_address_prefix       = "10.0.1.0/24"
-  destination_address_prefix  = "*"
-  resource_group_name         = azurerm_resource_group.example.name
-  network_security_group_name = azurerm_network_security_group.example.name
+  content                = file("${path.module}/install_phpmyadmin.sh")
 }
