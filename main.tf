e344e4fdc23ff86f01050f4bf86f9b1b3ec0514d
maria-ilinca <92468249+maria-ilinca@users.noreply.github.com>
2023-12-12 09:50:06 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,7 +1,24 @@
+# declare the random_string resource
+resource "random_string" "random_suffix" {
+  length  = 6
+  special = false
+}
 # Configure the Microsoft Azure Provider
+
+terraform{
+
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "=2.4.0"
+    }
+  }
+
+}
+
 provider "azurerm" {
   skip_provider_registration = true 
-  features = {}
+  features  {}
 }
 
 # Create a resource group
@@ -23,7 +40,7 @@ resource "azurerm_subnet" "public" {
   name                 = "public-subnet"
   resource_group_name  = azurerm_resource_group.ilinca.name
   virtual_network_name = azurerm_virtual_network.ilinca.name
-  address_prefixes     = ["10.0.1.0/24"]
+  address_prefix       = "10.0.1.0/24"
 }
 
 # Subnet Private
@@ -31,19 +48,21 @@ resource "azurerm_subnet" "private" {
   name                 = "private-subnet"
   resource_group_name  = azurerm_resource_group.ilinca.name
   virtual_network_name = azurerm_virtual_network.ilinca.name
-  address_prefixes     = ["10.0.2.0/24"]
+  address_prefix       = "10.0.2.0/24"
 }
 
 # Network Security Group for VM
 resource "azurerm_network_security_group" "ilinca" {
   name                = "ilinca-nsg"
   resource_group_name = azurerm_resource_group.ilinca.name
+  location            = azurerm_resource_group.ilinca.location
 }
 
 # Network Interface for VM
 resource "azurerm_network_interface" "ilinca" {
   name                = "ilinca-nic"
   resource_group_name = azurerm_resource_group.ilinca.name
+  location            = azurerm_resource_group.ilinca.location
 
   ip_configuration {
     name                          = "ilinca-nic-configuration"
@@ -57,32 +76,40 @@ resource "azurerm_virtual_machine" "ilinca_database" {
   name                  = "ilinca-database-vm"
   resource_group_name   = azurerm_resource_group.ilinca.name
   location              = azurerm_resource_group.ilinca.location
-  size                  = "Standard_DS1_v2"
-  admin_username        = "dbadmin"
-  admin_password        = "DBPassword1234!" 
+  vm_size               = "Standard_DS1_v2"
   network_interface_ids = [azurerm_network_interface.ilinca_database.id]
 
   os_profile {
     computer_name  = "ilinca-database-vm"
     admin_username = "dbadmin"
+    admin_password = "DBPassword1234!"
   }
 
   os_profile_linux_config {
     disable_password_authentication = false
   }
 
-  source_image_reference {
+  storage_image_reference {
     publisher = "Canonical"
     offer     = "UbuntuServer"
     sku       = "16.04-LTS"
     version   = "latest"
   }
+
+  storage_os_disk {
+    name              = "osdisk"
+    caching           = "ReadWrite"
+    create_option     = "FromImage"
+    managed_disk_type = "Premium_LRS"
+  }
 }
+
 
 # Network Interface for Database VM
 resource "azurerm_network_interface" "ilinca_database" {
   name                = "ilinca-database-nic"
   resource_group_name = azurerm_resource_group.ilinca.name
+  location            = azurerm_resource_group.ilinca.location
 
   ip_configuration {
     name                          = "ilinca-database-nic-configuration"
@@ -103,14 +130,13 @@ resource "azurerm_sql_server" "ilinca_sql_server" {
 
 # SQL Database
 resource "azurerm_sql_database" "ilinca_sql_db" {
-  name                        = "ilinca-sql-db"
-  resource_group_name         = azurerm_resource_group.ilinca.name
-  location                    = azurerm_resource_group.ilinca.location
-  server_name                 = azurerm_sql_server.ilinca_sql_server.name
-  edition                     = "Standard"
-  collation                   = "SQL_Latin1_General_CP1_CI_AS"
-  max_size_gb                 = 1
-  requested_service_objective = "S0"
+  name                   = "ilinca-sql-db"
+  resource_group_name    = azurerm_resource_group.ilinca.name
+  location               = azurerm_resource_group.ilinca.location
+  server_name            = azurerm_sql_server.ilinca_sql_server.name
+  edition                = "Standard"
+  collation              = "SQL_Latin1_General_CP1_CI_AS"
+  max_size_gb            = 1
 }
 
 # Virtual Machine
@@ -118,27 +144,34 @@ resource "azurerm_virtual_machine" "ilinca" {
   name                  = "ilinca-vm"
   resource_group_name   = azurerm_resource_group.ilinca.name
   location              = azurerm_resource_group.ilinca.location
-  size                  = "Standard_DS1_v2"
-  admin_username        = "admin"
-  admin_password        = "AdminPassword1234!"
-  network_interface_ids = [azurerm_network_interface.ilinca.id]
+  vm_size               = "Standard_DS1_v2"
+  network_interface_ids = [azurerm_network_interface.ilinca_database.id]
 
   os_profile {
     computer_name  = "ilinca-vm"
-    admin_username = "admin"
+    admin_username = "admin" 
+    admin_password = "AdminPassword1234!"
   }
 
   os_profile_linux_config {
     disable_password_authentication = false
   }
 
-  source_image_reference {
+  storage_os_disk {
+    name              = "osdisk"
+    caching           = "ReadWrite"
+    create_option     = "FromImage"
+    managed_disk_type = "Standard_LRS"
+  }
+
+  storage_image_reference {
     publisher = "Canonical"
     offer     = "UbuntuServer"
     sku       = "16.04-LTS"
     version   = "latest"
   }
 }
+
 
 # Apache VM Extension
 resource "azurerm_virtual_machine_extension" "phpmyadmin" {
@@ -159,11 +192,27 @@ SETTINGS
 PROTECTED_SETTINGS
 }
 
-# Custom Script for PHPMyAdmin installation
+# Storage Account for Scripts
+resource "azurerm_storage_account" "script_storage" {
+  name                     = "scriptstorage${random_string.random_suffix.result}"
+  resource_group_name      = azurerm_resource_group.ilinca.name
+  location                 = azurerm_resource_group.ilinca.location
+  account_tier             = "Standard"
+  account_replication_type = "LRS"
+}
+
+# Storage Container for Scripts
+resource "azurerm_storage_container" "script_container" {
+  name                  = "scriptcontainer"
+  storage_account_name  = azurerm_storage_account.script_storage.name
+  container_access_type = "private"
+}
+
+# Storage Blob for PHPMyAdmin installation script
 resource "azurerm_storage_blob" "install_phpmyadmin_script" {
   name                   = "install_phpmyadmin.sh"
   storage_account_name   = azurerm_storage_account.script_storage.name
   storage_container_name = azurerm_storage_container.script_container.name
   type                   = "Block"
-  content                = file("${path.module}/install_phpmyadmin.sh")
+  source                 = "${path.module}/install_phpmyadmin.sh"
 }
